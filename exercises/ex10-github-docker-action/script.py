bfcc97b61543fe090f216ce4db7fbefcf37a3d1d
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-10-18 19:32:10 +0200

    testing docker action

---
+++ exercises/ex10-github-docker-action/script.py
@@ -0,0 +1,13 @@
+import os
+import sys
+print(sys.argv)
+print("Github Docker Action - Yeah baby!")
+print("▄██   ▄      ▄████████    ▄████████    ▄█    █▄    ")
+print("███   ██▄   ███    ███   ███    ███   ███    ███   ")
+print("███▄▄▄███   ███    █▀    ███    ███   ███    ███   ")
+print("▀▀▀▀▀▀███  ▄███▄▄▄       ███    ███  ▄███▄▄▄▄███▄▄ ")
+print("▄██   ███ ▀▀███▀▀▀     ▀███████████ ▀▀███▀▀▀▀███▀  ")
+print("███   ███   ███    █▄    ███    ███   ███    ███   ")
+print("███   ███   ███    ███   ███    ███   ███    ███   ")
+print(" ▀█████▀    ██████████   ███    █▀    ███    █▀    ")
+os.system('echo "::set-output name=out::yeahyeah"')
